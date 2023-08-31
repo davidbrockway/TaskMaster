d193c187bf06d386dfabaff0778b10ef1a3ec112
davidbrockway <dav.brock@protonmail.com>
2023-08-31 02:27:47 +0100

    Update Style

--- my-app/src/components/AuthRoute.js
+++ my-app/src/components/AuthRoute.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Navigate} from "react-router-dom";
 
 function AuthRoute({ element }) {
-  // Replace this with your actual authentication check logic
+
   const isAuthenticated = localStorage.getItem("userId") !== null;
 
   
